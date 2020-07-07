1967300f850baa91404c22733593b87c76d85d5c
leo-tavares <leonardotavaresdev@gmail.com>
2020-07-06 22:05:56 -0300

    feat: should be able to show the total quantity of itens in the cart && should be able to calculate the cart total

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total = products.reduce((acc, product) => {
+      const subtotal = product.quantity * product.price;
+      return subtotal + acc;
+    }, 0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products.reduce((acc, product) => {
+      const productQuantity = product.quantity;
+      return productQuantity + acc;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
